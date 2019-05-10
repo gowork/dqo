eea55529447e9d0ce74d9915e786292db57bf9bf
Bronisław Białek <bronislaw.bialek@gowork.pl>
2019-05-10 18:08:48 +0200

    Remove useless uses

--- src/Generator/GenerateTables.php
+++ src/Generator/GenerateTables.php
@@ -5,12 +5,6 @@ namespace GW\DQO\Generator;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Schema\Table as DbalTable;
 use GW\Value\Wrap;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 class GenerateTables
 {
