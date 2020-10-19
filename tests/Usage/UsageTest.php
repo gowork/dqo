bdd3382ff99f0b50ab01708af817dfd70b179124
Bronisław Białek <after89@gmail.com>
2020-10-19 13:07:44 +0200

    Parametrize db tests

--- tests/Usage/UsageTest.php
+++ tests/Usage/UsageTest.php
@@ -4,6 +4,8 @@ namespace tests\GW\DQO\Usage;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Process\Process;
+use function getenv;
+use function sprintf;
 
 final class UsageTest extends TestCase
 {
@@ -14,21 +16,32 @@ final class UsageTest extends TestCase
         (new Process($command,  self::SYMFONY_DIR))->run();
     }
 
-    private function mustRunOnSymfonyDir(array $command): void
+    private function mustRunOnSymfonyDir(array $command, array $env = []): void
     {
         (new Process($command,  self::SYMFONY_DIR))->mustRun();
     }
 
     function test_real_app()
     {
+        $password = getenv('MYSQL_PASSWORD');
+        $env = [
+            'DATABASE_URL' => sprintf(
+                'mysql://%s:%s@%s/%s',
+                getenv('MYSQL_USER'),
+                $password === '_' ? '' : $password,
+                getenv('MYSQL_HOST'),
+                getenv('MYSQL_DATABASE'),
+            ),
+        ];
+
         $this->runOnSymfonyDIr(['rm', 'composer.lock']);
         $this->runOnSymfonyDIr(['rm', 'src/ClientRow.php']);
         $this->runOnSymfonyDIr(['rm', 'src/UserTable.php']);
         $this->runOnSymfonyDIr(['rm', 'src/UserRow.php']);
         $this->runOnSymfonyDIr(['rm', '-rf', 'vendor', 'repo']);
         $this->mustRunOnSymfonyDir(['composer', 'install']);
-        $this->mustRunOnSymfonyDir(['bin/console']);
-        $this->mustRunOnSymfonyDir(['bin/console', 'dqo:generate-tables', 'src', 'App', 'user']);
+        $this->mustRunOnSymfonyDir(['bin/console'], $env);
+        $this->mustRunOnSymfonyDir(['bin/console', 'dqo:generate-tables', 'src', 'App', 'user'], $env);
         self::assertFileExists(self::SYMFONY_DIR . 'src/ClientRow.php');
         self::assertFileExists(self::SYMFONY_DIR . 'src/UserTable.php');
         self::assertFileExists(self::SYMFONY_DIR . 'src/UserRow.php');
